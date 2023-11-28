7d687423c4363bb0759476840783d5dab8d8a4c8
JimmiXxX <jimmix139@gmail.com>
2023-11-28 14:56:26 +0300

    All calc

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 let selectId = document.querySelector('select')
 let priceEuro = document.querySelector('.priceEuro')
 let boldProf = document.querySelector('.boldProf')
-let workProf = document.querySelector('.workProf')
+let euroMat = document.querySelector('.euroMat')
+
+let tubeSize = document.querySelector('.tubeSize')
+let stubTube = document.querySelector('.stubTube')
+
+let workValue = document.querySelector('.workProfNumber')
+
+
+let A = document.querySelector('.widthA')
+let B = document.querySelector('.widthB')
+let C = document.querySelector('.widthC')
+let D = document.querySelector('.widthD')
+let H = document.querySelector('.heighS')
+let step = document.querySelector('.step')
+let lagName = document.querySelector('.lagName')
 
 selectId.addEventListener('change', function ()
 {
     let getValue = this.value
-    // console.log(getValue)
 
     if (getValue === 'Евроштакетник')
     {
@@ -21,7 +34,7 @@ selectId.addEventListener('change', function ()
                          <input type="number" placeholder="Введите значение"> <i>Руб.</i>
                      </span>
                      <span>Стоимость штакетника за 1 п.м
-                         <input type="number" placeholder="Введите значение"> <i>Руб.</i>
+                         <input type="number" value="180" placeholder="Введите значение"> <i>Руб.</i>
                      </span>
                      <span>Стоимость саморезов за 1 шт.
                          <input type="number" placeholder="Введите значение"> <i>Руб.</i>
@@ -32,9 +45,11 @@ selectId.addEventListener('change', function ()
             </div>
             `
 
+
         boldProf.style.display = "none"
-        workProf.style.display = "none"
+        euroMat.style.display = "flex"
         priceEuro.innerHTML = euro
+
     }
     else
     {
@@ -49,7 +64,7 @@ selectId.addEventListener('change', function ()
                          <input type="number" placeholder="Введите значение"> <i>Руб.</i>
                      </span>
                      <span>Стоимость профнастила за 1м2
-                         <input type="number" placeholder="Введите значение"> <i>Руб.</i>
+                         <input type="number" value="620" placeholder="Введите значение" class="priceProf"> <i>Руб.</i>
                      </span>
                      <span>Стоимость саморезов за 1 шт.
                          <input type="number" placeholder="Введите значение"> <i>Руб.</i>
@@ -60,9 +75,76 @@ selectId.addEventListener('change', function ()
             </div>
             `
         boldProf.style.display = "flex"
-        workProf.style.display = "flex"
         priceEuro.innerHTML = prof
+        euroMat.style.display = "none"
     }
 
 })
+tubeSize.addEventListener('change', function  ()
+{
+   if(tubeSize.value !== stubTube.value)
+   {
+       stubTube.value = tubeSize.value
+   }
+})
+stubTube.addEventListener('change', function  ()
+{
+   if(stubTube.value !== tubeSize.value)
+   {
+       tubeSize.value = stubTube.value
+   }
+})
+
+plusNumber = () =>
+{
+    if(workValue.value >= 2000){
+        alert('Слишком большое число!')
+    }
+    else{
+        let number = parseInt(workValue.value)
+        number += 50
+        workValue.value = number
+    }
+
+}
+minusNumber = () =>
+{
+    if(workValue.value <=100)
+    {
+        alert('Невозможно вывести значение в минус!')
+    }
+    else{
+    let numberMinus = parseInt(workValue.value)
+    numberMinus -= 50
+    workValue.value = numberMinus
+    }
+}
+
+
+let calc = () =>
+{
+    let a = parseInt(A.value)
+    let b = parseInt(B.value)
+    let c = parseInt(C.value)
+    let d = parseInt(D.value)
+    let per = a+b+c+d
+    let S = per * H.value
+    let valueStep = S/step.value - 4
+    console.log("Периметр: " + per)
+    console.log("Площадь: " + S)
+    console.log("Количество столбов и профнастила: " + valueStep)
+    let price = (valueStep/6)
+    let allPrice = (price + valueStep) * 620
+    let lagvalue = valueStep * +lagName.value
+
+    console.log("Количество саморезов: " + lagvalue* 5)
+    console.log("Количество лаг на забор: "+lagvalue)
+    console.log("Кол-во нахлеста: " + price)
+    console.log("Кол-во листов всего: " + (price+valueStep))
+    console.log("Стоимость профлистов с нахлестом: " + allPrice)
+
+}
+
+
+
 
