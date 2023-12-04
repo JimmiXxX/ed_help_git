26556e3ca2ceff69838d2e5779cab5ddd57a10b8
JimmiXxX <jimmix139@gmail.com>
2023-12-04 23:23:24 +0300

    Последнее обновление

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ let A = document.querySelector('.widthA')
 let B = document.querySelector('.widthB')
 let C = document.querySelector('.widthC')
 let D = document.querySelector('.widthD')
-let H = document.querySelector('.heighS')
+let H = document.querySelector('.heightS')
 let step = document.querySelector('.step')
-let lagName = document.querySelector('.lagName')
+let lagValue = document.querySelector('.lagValue')
+const lagOption = document.querySelector('.lagOption')
 
 selectId.addEventListener('change', function ()
 {
@@ -28,19 +29,19 @@ selectId.addEventListener('change', function ()
             <div class="euroPrice" style="padding-left: 15px; padding-top: 50px;">
                 <h1 style="padding-bottom: 50px;">Стоимость евроштакетника</h1>
                      <span>Стоимость столбов за 1 п.м
-                         <input type="number" placeholder="Введите значение"> <i>Руб.</i>
+                         <input type="number" placeholder="Введите значение" class="pillarPrice"> <i>Руб.</i>
                      </span>
                      <span>Стоимость лаг за 1 п.м
-                         <input type="number" placeholder="Введите значение"> <i>Руб.</i>
+                         <input type="number" placeholder="Введите значение" class="LagPrice"> <i>Руб.</i>
                      </span>
                      <span>Стоимость штакетника за 1 п.м
-                         <input type="number" value="180" placeholder="Введите значение"> <i>Руб.</i>
+                         <input type="number" value="180" placeholder="Введите значение" class="fencePrice"> <i>Руб.</i>
                      </span>
                      <span>Стоимость саморезов за 1 шт.
-                         <input type="number" placeholder="Введите значение"> <i>Руб.</i>
+                         <input type="number" placeholder="Введите значение" class="selfTappingScrewsPrice"> <i>Руб.</i>
                      </span>
                      <span>Стоимость монтажа за 1 п.м
-                         <input type="number" placeholder="Введите значение"> <i>Руб.</i>
+                         <input type="number" placeholder="Введите значение" class="InstallationPrice"> <i>Руб.</i>
                      </span>
             </div>
             `
@@ -57,20 +58,20 @@ selectId.addEventListener('change', function ()
             `
             <div class="profPrice" style="padding-left: 15px; padding-top: 50px;">
                 <h1 style="padding-bottom: 50px;">Стоимость профнастила</h1>
-                     <span>Стоимость столбов за 1 п.м
-                         <input type="number" placeholder="Введите значение"> <i>Руб.</i>
+                     <span>Стоимость столбов за 1м2
+                         <input type="number" placeholder="Введите значение" class="profPillarPrice"> <i>Руб.</i>
                      </span>
-                     <span>Стоимость лаг за 1 п.м
-                         <input type="number" placeholder="Введите значение"> <i>Руб.</i>
+                     <span>Стоимость лаг за 1м2
+                         <input type="number" placeholder="Введите значение" class="profLagPrice"> <i>Руб.</i>
                      </span>
                      <span>Стоимость профнастила за 1м2
-                         <input type="number" value="620" placeholder="Введите значение" class="priceProf"> <i>Руб.</i>
+                         <input type="number" value="620" placeholder="Введите значение" class="profPrice"> <i>Руб.</i>
                      </span>
                      <span>Стоимость саморезов за 1 шт.
-                         <input type="number" placeholder="Введите значение"> <i>Руб.</i>
+                         <input type="number" placeholder="Введите значение" class="selfTappingScrewsPrice"> <i>Руб.</i>
                      </span>
-                     <span>Стоимость монтажа за 1 п.м
-                         <input type="number" placeholder="Введите значение"> <i>Руб.</i>
+                     <span>Стоимость монтажа за 1м2
+                         <input type="number" placeholder="Введите значение" class="InstallationPrice"> <i>Руб.</i>
                      </span>
             </div>
             `
@@ -80,6 +81,8 @@ selectId.addEventListener('change', function ()
     }
 
 })
+
+
 tubeSize.addEventListener('change', function  ()
 {
    if(tubeSize.value !== stubTube.value)
@@ -121,27 +124,123 @@ minusNumber = () =>
 }
 
 
+
+const Pillar_Price = document.querySelector('.pillarPrice') // Объявление стоимости столбов у штакетника
+const Lag_Price = document.querySelector('.LagPrice') // Объявление стоимости лаг у штакетника
+const Fence_Price = document.querySelector('.fencePrice') // Объявление стоимости штакетника
+const SelfTappingScrews_Price = document.querySelector('.selfTappingScrewsPrice') // Объявление стоимости саморезов
+const Installation_Price = document.querySelector('.InstallationPrice') // Объявление стоимости монтажа
+const ProfPillar_Price = document.querySelector('.profPillarPrice') // Объявление стоимости столбов у профнастила
+const ProfLag_Price = document.querySelector('.profLagPrice') // Объявление стоимости лаг у профнастила
+const Prof_Price = document.querySelector('.profPrice') // Объявление стоимости профнастила
+
+
 let calc = () =>
 {
-    let a = parseInt(A.value)
-    let b = parseInt(B.value)
-    let c = parseInt(C.value)
-    let d = parseInt(D.value)
-    let per = a+b+c+d
-    let S = per * H.value
-    let valueStep = S/step.value - 4
-    console.log("Периметр: " + per)
-    console.log("Площадь: " + S)
-    console.log("Количество столбов и профнастила: " + valueStep)
-    let price = (valueStep/6)
-    let allPrice = (price + valueStep) * 620
-    let lagvalue = valueStep * +lagName.value
 
-    console.log("Количество саморезов: " + lagvalue* 5)
-    console.log("Количество лаг на забор: "+lagvalue)
-    console.log("Кол-во нахлеста: " + price)
-    console.log("Кол-во листов всего: " + (price+valueStep))
-    console.log("Стоимость профлистов с нахлестом: " + allPrice)
+    const aliases = {
+        "ГОСТ хуеты": 350,
+        "ГОСТ говна": 400,
+    }
+
+    let keys = Object.keys(aliases)
+    console.log(keys)
+
+    keys.forEach((element) =>{
+        const option = document.createElement("option")
+        option.setAttribute("value", element)
+        option.innerHTML = element
+        lagOption.appendChild(option)
+        })
+
+    for(let key in aliases)
+    {
+        console.log(key)
+        let test = aliases[key]
+        console.log(test)
+    }
+
+
+    // const price = aliases[selectValue]
+
+
+
+
+
+    // let store = lagOption.selectedIndex
+    // let price = 0
+    //
+    // switch (store)
+    // {
+    //     case 0:
+    //     {
+    //         price = 638
+    //         break
+    //     }
+    //     case 1:
+    //     {
+    //         price = 104
+    //         break
+    //     }
+    //     case 2:
+    //     {
+    //         price = 996
+    //         break
+    //     }
+    //     case 3:
+    //     {
+    //         price = 765
+    //         break
+    //     }
+    //     case 4:
+    //     {
+    //         price = 366
+    //         break
+    //     }
+    //     case 5:
+    //     {
+    //         price = 269
+    //         break
+    //     }
+    //     case 6:
+    //     {
+    //         price = 160
+    //         break
+    //     }
+    //     case 7:
+    //     {
+    //         price = 335
+    //         break
+    //     }
+    //     case 8:
+    //     {
+    //         price = 464
+    //         break
+    //     }
+    //     case 9:
+    //     {
+    //         price = 366
+    //         break
+    //     }
+    // }
+
+
+    const Perimeter = Number(A.value) + Number(B.value)  // Расчет периметра
+    const Square = Perimeter * H.value  // Расчет площади (периметр * высота столбов)
+    const valueStep = Square/step.value - 4 // Количество столбов и профлистов
+    const Overlap = Math.floor(valueStep/6) // Количество нахлеста
+    const PriceOverlap = (Overlap + valueStep) * 620 // Цена профнастила с нахлестом
+    const lag = valueStep * lagValue.value // Количество лаг на весь забор
+    const SelfTappingScrewsValue = lag * 5 // Количество саморезов
+
+    // console.log("Периметр: " + Perimeter)
+    // console.log("Площадь: " + Square)
+    // console.log("Количество столбов и профнастила: " + valueStep)
+    // console.log("Количество саморезов: " + SelfTappingScrewsValue)
+    // console.log("Количество лаг на забор: " + lag)
+    // console.log("Кол-во нахлеста: " + Overlap)
+    // console.log("Кол-во листов всего: " + (Overlap+valueStep))
+    // console.log("Стоимость профлистов с нахлестом: " + PriceOverlap)
 
 }
 
