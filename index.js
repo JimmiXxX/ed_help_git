7efe232eb5f3d19e0e6e06425dc9861ec1e527c7
JimmiXxX <jimmix139@gmail.com>
2023-12-13 15:34:21 +0300

    Тест

--- index.js
+++ index.js
@@ -1,248 +1,203 @@
-let selectId = document.querySelector('select')
-let priceEuro = document.querySelector('.priceEuro')
-let boldProf = document.querySelector('.boldProf')
-let euroMat = document.querySelector('.euroMat')
+const selectId = document.querySelector( '.MainType' )
+const priceEuro = document.querySelector( '.priceEuro' )
+const boldProf = document.querySelector( '.boldProf' )
+const euroMat = document.querySelector( '.euroMat' )
+const hiddenMenu = document.querySelector( '.hiddenMenu' )
+const euroBlock = document.querySelector( '.euroBlock' )
+const profBlock = document.querySelector( '.profBlock' )
+const workProf = document.querySelector( '.workProf' )
+const dataCreate = document.querySelector('.dataCreate')
 
-let tubeSize = document.querySelector('.tubeSize')
-let stubTube = document.querySelector('.stubTube')
+const content = document.querySelector( '.content' )
+const tubeSize = document.querySelector( '.tubeSize' ) //Труба для столбов
+const stubTube = document.querySelector( '.stubTube' ) //Заглушки на столбы
+const workValue = document.querySelector( '.workProfNumber' )
+const printCalcResult = document.querySelector( '.printCalcResult' )
 
-let workValue = document.querySelector('.workProfNumber')
+const Pillar_Price = document.querySelector( '.pillarPrice' ) // Объявление стоимости столбов у штакетника
+const Lag_Price = document.querySelector( '.LagPrice' ) // Объявление стоимости лаг у штакетника
+const Fence_Price = document.querySelector( '.fencePrice' ) // Объявление стоимости штакетника
+const SelfTappingScrews_Price = document.querySelector( '.selfTappingScrewsPrice' ) // Объявление стоимости саморезов
+const ProfPillar_Price = document.querySelector( '.profPillarPrice' ) // Объявление стоимости столбов у профнастила
+const ProfLag_Price = document.querySelector( '.profLagPrice' ) // Объявление стоимости лаг у профнастила
+const Prof_Price = document.querySelector( '.profPrice' ) // Объявление стоимости профнастила
+const InstallationPrice = document.querySelector( '.InstallationPrice_Prof' ) // Объявление стоимости монтажа
+
+let A = document.querySelector( '.widthA' )
+let B = document.querySelector( '.widthB' )
+let C = document.querySelector( '.widthC' )
+let D = document.querySelector( '.widthD' )
+let H = document.querySelector( '.heightS' )
+let step = document.querySelector( '.step' )
+let lagValue = document.querySelector( '.lagValue' )
+const lagOption = document.querySelector( '.lagOption' )
 
 
-let A = document.querySelector('.widthA')
-let B = document.querySelector('.widthB')
-let C = document.querySelector('.widthC')
-let D = document.querySelector('.widthD')
-let H = document.querySelector('.heightS')
-let step = document.querySelector('.step')
-let lagValue = document.querySelector('.lagValue')
-const lagOption = document.querySelector('.lagOption')
-
-selectId.addEventListener('change', function ()
-{
+selectId.addEventListener( 'change', function () {
     let getValue = this.value
-
-    if (getValue === 'Евроштакетник')
-    {
-        let euro =
-            `
-            <div class="euroPrice" style="padding-left: 15px; padding-top: 50px;">
-                <h1 style="padding-bottom: 50px;">Стоимость евроштакетника</h1>
-                     <span>Стоимость столбов за 1 п.м
-                         <input type="number" placeholder="Введите значение" class="pillarPrice"> <i>Руб.</i>
-                     </span>
-                     <span>Стоимость лаг за 1 п.м
-                         <input type="number" placeholder="Введите значение" class="LagPrice"> <i>Руб.</i>
-                     </span>
-                     <span>Стоимость штакетника за 1 п.м
-                         <input type="number" value="180" placeholder="Введите значение" class="fencePrice"> <i>Руб.</i>
-                     </span>
-                     <span>Стоимость саморезов за 1 шт.
-                         <input type="number" placeholder="Введите значение" class="selfTappingScrewsPrice"> <i>Руб.</i>
-                     </span>
-                     <span>Стоимость монтажа за 1 п.м
-                         <input type="number" placeholder="Введите значение" class="InstallationPrice"> <i>Руб.</i>
-                     </span>
-            </div>
-            `
-
-
+    if ( getValue === 'Евроштакетник' ) {
+        euroBlock.style.display = 'block'
+        profBlock.style.display = 'none'
+        hiddenMenu.style.display = 'block'
         boldProf.style.display = "none"
         euroMat.style.display = "flex"
-        priceEuro.innerHTML = euro
-
-    }
-    else
-    {
-        let prof =
-            `
-            <div class="profPrice" style="padding-left: 15px; padding-top: 50px;">
-                <h1 style="padding-bottom: 50px;">Стоимость профнастила</h1>
-                     <span>Стоимость столбов за 1м2
-                         <input type="number" placeholder="Введите значение" class="profPillarPrice"> <i>Руб.</i>
-                     </span>
-                     <span>Стоимость лаг за 1м2
-                         <input type="number" placeholder="Введите значение" class="profLagPrice"> <i>Руб.</i>
-                     </span>
-                     <span>Стоимость профнастила за 1м2
-                         <input type="number" value="620" placeholder="Введите значение" class="profPrice"> <i>Руб.</i>
-                     </span>
-                     <span>Стоимость саморезов за 1 шт.
-                         <input type="number" placeholder="Введите значение" class="selfTappingScrewsPrice"> <i>Руб.</i>
-                     </span>
-                     <span>Стоимость монтажа за 1м2
-                         <input type="number" placeholder="Введите значение" class="InstallationPrice"> <i>Руб.</i>
-                     </span>
-            </div>
-            `
+        workProf.style.display = 'none'
+    } else {
+        euroBlock.style.display = 'none'
+        profBlock.style.display = 'block'
+        hiddenMenu.style.display = 'block'
         boldProf.style.display = "flex"
-        priceEuro.innerHTML = prof
         euroMat.style.display = "none"
+        workProf.style.display = 'flex'
     }
+} )
 
-})
 
-
-tubeSize.addEventListener('change', function  ()
-{
-   if(tubeSize.value !== stubTube.value)
-   {
-       stubTube.value = tubeSize.value
-   }
-})
-stubTube.addEventListener('change', function  ()
-{
-   if(stubTube.value !== tubeSize.value)
-   {
-       tubeSize.value = stubTube.value
-   }
-})
-
-plusNumber = () =>
-{
-    if(workValue.value >= 2000){
-        alert('Слишком большое число!')
-    }
-    else{
-        let number = parseInt(workValue.value)
+plusNumber = () => {
+    if ( workValue.value >= 2000 ) {
+        workValue.value = 150
+    } else {
+        let number = parseInt( workValue.value )
         number += 50
         workValue.value = number
     }
 
 }
-minusNumber = () =>
-{
-    if(workValue.value <=100)
-    {
-        alert('Невозможно вывести значение в минус!')
-    }
-    else{
-    let numberMinus = parseInt(workValue.value)
-    numberMinus -= 50
-    workValue.value = numberMinus
+minusNumber = () => {
+    if ( workValue.value <= 100 ) {
+        workValue.value = 2000
+    } else {
+        let numberMinus = parseInt( workValue.value )
+        numberMinus -= 50
+        workValue.value = numberMinus
     }
 }
 
+const lagAllSize = {
+    "50х50х2х6000": 366,
+    "40х40х1.5х6000": 225,
+    "40х20х1.5х6000": 164,
+    "25х25х1.5х6000": 140,
+    "20х20х1.5х6000": 104,
+    "15х15х1.5х6000": 75,
+}
+
+const tubeAllSize = {
+    "60*60*2,0": 638,
+    "20*20*1,5": 104,
+    "80*80*4,0": 996,
+    "80*80*3,0х12000": 765,
+    "50*50*2,0х6000": 366,
+    "40*40*2,0х6000": 269,
+    "30*30*2,0х6000": 160,
+    "50*25*1,5": 335,
+    "60*40*3х6000": 464,
+    "50*50*2,0": 366,
+}
+let keysTube = Object.keys( tubeAllSize )
+let keysStube = Object.keys( tubeAllSize )
+keysTube.forEach( ( key ) => {
+    const tubeSizes = document.createElement( 'option' )
+    tubeSizes.setAttribute( 'value', key )
+    tubeSizes.innerHTML = key
+    tubeSize.appendChild( tubeSizes )
+} )
+keysStube.forEach( ( key ) => {
+    const tubeSizes = document.createElement( 'option' )
+    tubeSizes.setAttribute( 'value', key )
+    tubeSizes.innerHTML = key
+    stubTube.appendChild( tubeSizes )
+} )
 
 
-const Pillar_Price = document.querySelector('.pillarPrice') // Объявление стоимости столбов у штакетника
-const Lag_Price = document.querySelector('.LagPrice') // Объявление стоимости лаг у штакетника
-const Fence_Price = document.querySelector('.fencePrice') // Объявление стоимости штакетника
-const SelfTappingScrews_Price = document.querySelector('.selfTappingScrewsPrice') // Объявление стоимости саморезов
-const Installation_Price = document.querySelector('.InstallationPrice') // Объявление стоимости монтажа
-const ProfPillar_Price = document.querySelector('.profPillarPrice') // Объявление стоимости столбов у профнастила
-const ProfLag_Price = document.querySelector('.profLagPrice') // Объявление стоимости лаг у профнастила
-const Prof_Price = document.querySelector('.profPrice') // Объявление стоимости профнастила
+let keysLag = Object.keys( lagAllSize )
+keysLag.forEach( ( keys ) => {
+    const option = document.createElement( "option" )
+    option.setAttribute( "value", keys )
+    option.innerHTML = keys
+    lagOption.appendChild( option )
+
+} )
 
 
-let calc = () =>
-{
+tubeSize.value = stubTube.value
 
-    const aliases = {
-        "ГОСТ хуеты": 350,
-        "ГОСТ говна": 400,
+tubeSize.addEventListener( 'change', () => {
+    if ( tubeSize.value !== stubTube.value ) {
+        stubTube.value = tubeSize.value
     }
-
-    let keys = Object.keys(aliases)
-    console.log(keys)
-
-    keys.forEach((element) =>{
-        const option = document.createElement("option")
-        option.setAttribute("value", element)
-        option.innerHTML = element
-        lagOption.appendChild(option)
-        })
-
-    for(let key in aliases)
-    {
-        console.log(key)
-        let test = aliases[key]
-        console.log(test)
+} )
+stubTube.addEventListener( 'change', () => {
+    if ( stubTube.value !== tubeSize.value ) {
+        tubeSize.value = stubTube.value
     }
+} )
+
+tubeSize.addEventListener( 'change', () => {
+    let ValuePriceProf = tubeAllSize[tubeSize.value]
+    ProfPillar_Price.value = ValuePriceProf
+    Pillar_Price.value = ValuePriceProf
+} )
+stubTube.addEventListener( 'change', () => {
+    let ValuePriceProf = tubeAllSize[tubeSize.value]
+    ProfPillar_Price.value = ValuePriceProf
+    Pillar_Price.value = ValuePriceProf
+} )
+
+lagOption.addEventListener( 'change', () => {
+    ProfLag_Price.value = lagAllSize[lagOption.value]
+} )
 
 
-    // const price = aliases[selectValue]
+let options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    timezone: 'UTC',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+}
 
 
+const calcTube = () => {
 
+    printCalcResult.style.display = 'block'
 
-
-    // let store = lagOption.selectedIndex
-    // let price = 0
-    //
-    // switch (store)
-    // {
-    //     case 0:
-    //     {
-    //         price = 638
-    //         break
-    //     }
-    //     case 1:
-    //     {
-    //         price = 104
-    //         break
-    //     }
-    //     case 2:
-    //     {
-    //         price = 996
-    //         break
-    //     }
-    //     case 3:
-    //     {
-    //         price = 765
-    //         break
-    //     }
-    //     case 4:
-    //     {
-    //         price = 366
-    //         break
-    //     }
-    //     case 5:
-    //     {
-    //         price = 269
-    //         break
-    //     }
-    //     case 6:
-    //     {
-    //         price = 160
-    //         break
-    //     }
-    //     case 7:
-    //     {
-    //         price = 335
-    //         break
-    //     }
-    //     case 8:
-    //     {
-    //         price = 464
-    //         break
-    //     }
-    //     case 9:
-    //     {
-    //         price = 366
-    //         break
-    //     }
-    // }
-
-
-    const Perimeter = Number(A.value) + Number(B.value)  // Расчет периметра
+    const Perimeter = Number( A.value ) + Number( B.value ) + Number( C.value ) + Number( D.value ) // Расчет периметра
     const Square = Perimeter * H.value  // Расчет площади (периметр * высота столбов)
-    const valueStep = Square/step.value - 4 // Количество столбов и профлистов
-    const Overlap = Math.floor(valueStep/6) // Количество нахлеста
-    const PriceOverlap = (Overlap + valueStep) * 620 // Цена профнастила с нахлестом
+    const valueStep = Square / step.value - 4 // Количество столбов и профлистов
+    const Overlap = Math.ceil( valueStep / 6 ) // Количество нахлеста
+    const PriceOverlap = (Overlap + valueStep) * ProfLag_Price.value // Цена профнастила с нахлестом
     const lag = valueStep * lagValue.value // Количество лаг на весь забор
     const SelfTappingScrewsValue = lag * 5 // Количество саморезов
+    const priceInstall = InstallationPrice.value * Perimeter
+    dataCreate.innerHTML = new Date().toLocaleDateString('ru', options)
 
-    // console.log("Периметр: " + Perimeter)
-    // console.log("Площадь: " + Square)
-    // console.log("Количество столбов и профнастила: " + valueStep)
-    // console.log("Количество саморезов: " + SelfTappingScrewsValue)
-    // console.log("Количество лаг на забор: " + lag)
-    // console.log("Кол-во нахлеста: " + Overlap)
-    // console.log("Кол-во листов всего: " + (Overlap+valueStep))
-    // console.log("Стоимость профлистов с нахлестом: " + PriceOverlap)
+    console.log( InstallationPrice.value )
+    const allPrice = PriceOverlap + (SelfTappingScrewsValue * 5) + priceInstall + (valueStep * ProfPillar_Price.value) + (valueStep * lagAllSize[lagOption.value])
 
+
+    document.querySelector( '.resultPerimeter' ).innerHTML = Perimeter
+    document.querySelector( '.resultPlaza' ).innerHTML = Square
+    document.querySelector( '.resultPillar' ).innerHTML = valueStep
+    document.querySelector( '.resultSelfTapping' ).innerHTML = SelfTappingScrewsValue
+    document.querySelector( '.resultLag' ).innerHTML = lag
+    document.querySelector( '.resultList' ).innerHTML = (Overlap + valueStep)
+
+    document.querySelector( '.priceProf' ).innerHTML = PriceOverlap
+    document.querySelector( '.priceSelfTapping' ).innerHTML = SelfTappingScrewsValue * 5
+    document.querySelector( '.priceInstallation' ).innerHTML = priceInstall
+    document.querySelector( '.pricePillar' ).innerHTML = valueStep * ProfPillar_Price.value
+    document.querySelector( '.priceLag' ).innerHTML = valueStep * lagAllSize[lagOption.value]
+    document.querySelector( '.priceAll' ).innerHTML = allPrice
 }
+
+
+
+
+
+
 
 
 
